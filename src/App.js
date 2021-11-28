18f70a5c2492c7bf654c503e2e1d8af0f9663ca1
thishan <iroshan.gk@gmail.com>
2021-11-29 00:16:33 +0530

    fomik

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Routes ,Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
-
 
 import Nav from "./nav/Nav";
 import Table from "./Table/Table";
 import AddPage from "./addPage/Testpage";
+import Page from "./addPage/Addpage";
 import Edit from "./editPage/EditPage";
-
 
 export default function App() {
   return (
     <>
-    <div className="md:ml-64">
-    <Nav />
-      <Routes >
-      <Route path='/' element={<Table/>} />
-      <Route path='/addpage' element={<AddPage/>} />
-      <Route path="/Edit-todo/:id" exact element={ <Edit/> } />
-      </Routes>
-    </div>
-  </>
+      <div className="md:ml-64">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Table />} />
+          <Route path="/addpage" element={<AddPage />} />
+          <Route path="/add-page" element={<Page />} />
+          <Route path="/Edit-todo/:id" exact element={<Edit />} />
+        </Routes>
+      </div>
+    </>
   );
 }
---
+++ text.md
@@ -0,0 +1,3 @@
+https://www.bezkoder.com/react-form-validation-example-formik/
+https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/
+https://www.javascripttutorial.net/dom/events/add-an-event-handler/
